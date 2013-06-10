3773ca22f3d1d0582e204f8a33d96542856ffd95
Mohammad Hasani Eghtedar <m.h.eghtedar@gmail.com>
2013-06-10 05:35:22 -0700

    Updated ms-My translation Fixed include helper for php 4

--- mod_currentdatetime.php
+++ mod_currentdatetime.php
@@ -10,7 +10,7 @@
 defined('_JEXEC') or die;
 
 // Include the syndicate functions only once
-require_once __DIR__ . '/helper.php';
+require_once dirname(__FILE__) . '/helper.php';
 
 $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 
